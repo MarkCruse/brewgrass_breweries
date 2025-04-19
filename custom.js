8446999ffb4e166e8960aa38a371472f4be41843
Mark Cruse <mark.cruse@uky.edu>
2025-04-19 14:10:37 -0400

    ignore DS_store and secure the token

--- custom.js
+++ custom.js
@@ -2,7 +2,7 @@ var map = L.map("map").setView([38.0336, -84.5149], 14); // Set the initial map 
 
 // Add the Mapbox Streets tile layer
 L.tileLayer(
-  "https://api.mapbox.com/styles/v1/mdcruse/cljncp1g700lv01pd8f7g00fj/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWRjcnVzZSIsImEiOiJjanZvN25kaHQxdzAxNDhwZjM4NDNvMXV4In0.s4GSawMNB7Jo4Vf7LXKEew",
+  "https://api.mapbox.com/styles/v1/mdcruse/cljncp1g700lv01pd8f7g00fj/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWRjcnVzZSIsImEiOiJjbTlvZGljZmIweWFmMmlwdjhxc3d0anl2In0.uPUj9yZh0yPjwjiApe7bOw",
   {
     maxZoom: 19,
     attribution: 'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
